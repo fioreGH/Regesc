155a9db4cbc6fc1c646f351fc7171144d3903274
fioreGH <rfiore@gmail.com>
2024-08-22 16:39:51 -0300

    Alterar dados do professor

--- src/main/java/br/com/escola/regesc/service/CrudProfessorService.java
+++ src/main/java/br/com/escola/regesc/service/CrudProfessorService.java
@@ -2,8 +2,10 @@ package br.com.escola.regesc.service;
 
 import br.com.escola.regesc.orm.Professor;
 import br.com.escola.regesc.repository.ProfessorRepository;
+import ch.qos.logback.core.joran.conditional.ElseAction;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 @Service
@@ -24,12 +26,16 @@ public class CrudProfessorService {
             System.out.println("\nQual ação deseja executar?");
             System.out.println("0 - voltar ao menu anterior");
             System.out.println("1 - cadastrar novo professor");
+            System.out.println("2 - atualizar um professor");
 
             int opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
                     this.cadastrarProfessor(scanner);
+                    break;
+                case 2:
+                    this.atualizarProfessor(scanner);
                     break;
                 default:
                     isTrue = false;
@@ -51,6 +57,38 @@ public class CrudProfessorService {
 
         this.professorRepository.save(professor);
         System.out.println("Professor salvo com sucesso!\n");
+
+    }
+
+    private void atualizarProfessor(Scanner scanner){
+
+        System.out.print("Digite o id do Professor a ser atualizado: ");
+        Long id = scanner.nextLong();
+
+        Optional<Professor> optional = this.professorRepository.findById(id);
+
+        if (optional.isPresent()){
+
+            Professor professor = optional.get();
+
+            System.out.print("Digite o nome do Professor: ");
+            String nome = scanner.next();
+
+            System.out.print("Digite o prontuário do Professor: ");
+            String prontuario = scanner.next();
+
+            professor.setNome(nome);
+            professor.setProntuario(prontuario);
+
+            professorRepository.save(professor);
+
+            System.out.println("Professor atualizado com sucesso!\n");
+
+        }
+        else{
+            System.out.print("O id informado: " + id + " é invalido\n");
+        }
+
 
     }
 
